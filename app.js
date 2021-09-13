4b4bbf9adf72d92948a60bc547a42bc2fd755f9b
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-09-13 23:09:50 +0530

    added upto template routes

--- app.js
+++ app.js
@@ -20,25 +20,58 @@ app.get("/",function(req,res){
     res.send("<h1>Hello from Backend</h1>");
 });
 
+
+let users = {
+
+};
 let obj = {
-    name:"harsh",
+    name:"harshit",
+    age:29
 }
 
 
+//create
+app.post("/user",function(req,res){
+    console.log("req.data",req.body);
+    users = req.body;
+    // res.status(200).json(req.body);
+    res.status(200).send("data received and user added");
+});
+
 //getting data from server
+//get/read
 app.get("/user",function(req,res){
     console.log("users");
     // for sending ke val pairs to frontend
-    res.json(obj);
+    res.json(users);
 });
 
 
 //giving data to server
-app.post("/user",function(req,res){
-    console.log("req.data",req.body);
-    // res.status(200).json(req.body);
-    res.status(200).send("data received");
+
+
+//patch is update
+app.patch("/user",function(req,res){
+    let obj = req.body;
+    for(let key in obj){
+        users[key] = obj[key]
+    }
+    res.status(200).json(users);
 });
+
+//delete
+app.delete("/user",function(req,res){
+    users = {};
+    res.status(200).json(users);
+});
+
+
+// multiple routes
+//inko bolte hain template routes
+app.get("/user/:id",function(req,res){
+    console.log(req.params);
+    res.status(200).send("Apka data");
+})
 
 app.listen(8080,function(){
     console.log("server started");
