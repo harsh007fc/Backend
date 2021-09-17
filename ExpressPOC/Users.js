25392b262e9876d16cb177b032957a83a8b78152
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-09-17 11:46:10 +0530

    added upto middleware

---
+++ ExpressPOC/Users.js
@@ -0,0 +1,20 @@
+let users = [{
+    id: 1,
+    name: "Harsh",
+    email: "dummy@mail.com",
+    status: "active",
+},
+{
+    id: 2,
+    name: "Harshit",
+    email: "dummy1@mail.com",
+    status: "active"
+}, {
+    id: 3,
+    name: "Harshita",
+    email: "dummy2@mail.com",
+    status: "active"
+},
+];
+
+module.exports = users;
---
+++ ExpressPOC/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const users = require('./users')
+
+
+
+//we dont do that here manually
+// app.get("/",function(req,res){
+//     // res.send("<h1>Hello from Backend</h1>");
+//     console.log(__dirname);
+//     res.sendFile(path.join(__dirname,'Public','index.html'));
+// });
+
+//set static folder
+app.use(express.static('public'));
+
+//get all users
+
+
+
+const logger = (req,res,next) => {
+    console.log( new Date().toISOString());
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    next();
+}
+
+//init logger middleware
+app.use(logger);
+
+app.get('/api/users',(req,res)=> res.json(users));
+
+//set port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server Started at ${PORT}`));
